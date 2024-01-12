2f9b0e8e9adb7a2b21e4d200b8fac95e25ec69a7
Phong Nguyen <phong.nt.26101993@gmail.com>
2024-01-12 18:38:23 +0800

    Data Factory Trigger Event

--- DataFactory/setup.sh
+++ DataFactory/setup.sh
@@ -5,6 +5,7 @@ AzureDataLakeStorageGen2LinkedService=ls_adls_covidreportingdl
 AzureDataFactorySinkDataSet=ds_population_raw_tsv
 
 AzureDataFactoryPipeline=pl_ingest_population_data
+AzureDataFactoryTrigger=tr_ingest_population_data
 
 az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --linked-service-name $AzureStorageLinkedService --properties @LinkedService/AzureStorageLinkedService.json
 az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --linked-service-name $AzureDataLakeStorageGen2LinkedService --properties @LinkedService/AzureDataLakeGen2LinkedService.json
@@ -12,4 +13,5 @@ az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySourceDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySourceDataSet.json
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySinkDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySinkDataSet.json
 
-az datafactory pipeline create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --pipeline @Pipeline/AzureDataFactoryCopyPipeline.json
+az datafactory pipeline create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryPipeline --pipeline @Pipeline/AzureDataFactoryCopyPipeline.json
+az datafactory trigger create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryTrigger --properties @Pipeline/AzureDataFactoryTriggerEvent.json
