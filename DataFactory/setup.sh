31ca756bd72c73770c68e48e799e9d6a9fc0db74
Phong Nguyen <phong.nt.26101993@gmail.com>
2024-01-12 15:03:52 +0800

    + Update Pipeline

--- DataFactory/setup.sh
+++ DataFactory/setup.sh
@@ -4,7 +4,12 @@ AzureDataFactorySourceDataSet=ds_population_raw_gz
 AzureDataLakeStorageGen2LinkedService=ls_adls_covidreportingdl
 AzureDataFactorySinkDataSet=ds_population_raw_tsv
 
+AzureDataFactoryPipeline=pl_ingest_population_data
+
 az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --linked-service-name $AzureStorageLinkedService --properties @LinkedService/AzureStorageLinkedService.json
 az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --linked-service-name $AzureDataLakeStorageGen2LinkedService --properties @LinkedService/AzureDataLakeGen2LinkedService.json
 
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySourceDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySourceDataSet.json
+az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySinkDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySinkDataSet.json
+
+az datafactory pipeline create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --pipeline @Pipeline/AzureDataFactoryCopyPipeline.json
