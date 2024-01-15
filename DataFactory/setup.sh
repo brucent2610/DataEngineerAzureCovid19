31e8e542c593b785e7bda36cbbf17947736aaaca
Phong Nguyen <phong.nt.26101993@gmail.com>
2024-01-15 15:38:21 +0800

    + Finish the ECDC Open Data Pipeline

--- DataFactory/setup.sh
+++ DataFactory/setup.sh
@@ -13,16 +13,24 @@ az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySourceDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySourceDataSet.json
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySinkDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySinkDataSet.json
 
-az datafactory pipeline create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryPipeline --pipeline @Pipeline/AzureDataFactoryCopyPipeline.json
 az datafactory trigger create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryTrigger --properties @Pipeline/AzureDataFactoryTriggerEvent.json
+az datafactory pipeline create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryPipeline --pipeline @Pipeline/AzureDataFactoryCopyPipeline.json
 
 
 AzureStorageLinkedServiceOpenData=ls_http_opendata_ecdc_europa_eu
 AzureDataFactorySourceDataSetOpenData=ds_cases_deaths_raw_csv_http
+AzureDataFactoryConfigDataSet=ds_ecdc_file_list
 
 AzureDataFactorySinkDataSetOpenData=ds_cases_deaths_raw_csv_dl
 
+AzureDataFactoryOpenDataPipeline=pl_ingest_ecdc_data
+AzureDataFactoryOpenDataTrigger=tr_ingest_ecdc_data
+
 az datafactory linked-service create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --linked-service-name $AzureStorageLinkedServiceOpenData --properties @LinkedService/AzureStorageLinkedServiceOpenData.json
 
+az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactoryConfigDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactoryConfigDataSetOpenData.json
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySourceDataSet --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySourceDataSetOpenData.json
 az datafactory dataset create --resource-group $RESOURCE_GROUP --dataset-name $AzureDataFactorySinkDataSetOpenData --factory-name $FACTORY_NAME --properties @DataSet/AzureDataFactorySinkDataSetOpenData.json
+
+az datafactory trigger create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryOpenDataTrigger --properties @Pipeline/AzureDataFactoryOpenDataTriggerEvent.json
+az datafactory pipeline create --resource-group $RESOURCE_GROUP --factory-name $FACTORY_NAME --name $AzureDataFactoryOpenDataPipeline --pipeline @Pipeline/AzureDataFactoryCopyPipelineOpenData.json
