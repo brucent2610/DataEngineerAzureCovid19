961092c5f1961503e2019be5495d5f87f9f5f3f3
Phong Nguyen <phong.nt.26101993@gmail.com>
2024-01-27 17:57:47 +0800

    + Update Copy Data to SQL

--- Command/setup.sh
+++ Command/setup.sh
@@ -24,17 +24,22 @@ az storage account create --name $DATALAKE_STORAGE_GEN2_NAME --resource-group $R
 az sql server create --name $DATABASE_SQL_SERVER_NAME --resource-group $RESOURCE_GROUP --location $LOCATION --admin-user $DATABASE_SQL_SERVER_ADMIN_USER --admin-password $DATABASE_SQL_SERVER_ADMIN_PASSWORD
 az sql db create --resource-group $RESOURCE_GROUP --server $DATABASE_SQL_SERVER_NAME --name $DATABASE_SQL_NAME --edition $DATABASE_SQL_EDITION --max-size 2GB --capacity $DATABASE_SQL_CAPACITY --compute-model Provisioned --backup-storage-redundancy $DATABASE_SQL_STORAGE_REDUNDANCY
 
-
 # Data Ingestion from Azure Blob
 STORAGE_ACCOUNT_CONTAINER_NAME=population
 STORAGE_ACCOUNT_CONTAINER_CONFIGS_NAME=configs
+STORAGE_ACCOUNT_CONTAINER_SCRIPTS_NAME=scripts
 SOURCE_FILE_NAME=population_by_age.tsv.gz
 
-DATALAKE_STORAGE_GEN2_CONTAINER_NAME=raw
+DATALAKE_STORAGE_GEN2_CONTAINER_RAW_NAME=raw
+DATALAKE_STORAGE_GEN2_CONTAINER_LOOKUP_NAME=lookup
+DATALAKE_STORAGE_GEN2_CONTAINER_PROCESSED_NAME=processed
 
 az storage container create --account-name $STORAGE_ACCOUNT_NAME --name $STORAGE_ACCOUNT_CONTAINER_NAME --auth-mode login
 az storage container create --account-name $STORAGE_ACCOUNT_NAME --name $STORAGE_ACCOUNT_CONTAINER_CONFIGS_NAME --auth-mode login
+az storage container create --account-name $STORAGE_ACCOUNT_NAME --name $STORAGE_ACCOUNT_CONTAINER_SCRIPTS_NAME --auth-mode login
 az storage blob upload --account-name $STORAGE_ACCOUNT_NAME --container-name $STORAGE_ACCOUNT_CONTAINER_NAME --name $SOURCE_FILE_NAME --file $SOURCE_FILE_NAME --auth-mode login
 
-az storage container create --account-name $DATALAKE_STORAGE_GEN2_NAME --name $DATALAKE_STORAGE_GEN2_CONTAINER_NAME --auth-mode login
+az storage container create --account-name $DATALAKE_STORAGE_GEN2_NAME --name $DATALAKE_STORAGE_GEN2_CONTAINER_RAW_NAME --auth-mode login
+az storage container create --account-name $DATALAKE_STORAGE_GEN2_NAME --name $DATALAKE_STORAGE_GEN2_CONTAINER_LOOKUP_NAME --auth-mode login
+az storage container create --account-name $DATALAKE_STORAGE_GEN2_NAME --name $DATALAKE_STORAGE_GEN2_CONTAINER_PROCESSED_NAME --auth-mode login
 
---
+++ SQLDatabase/setup.sh
@@ -0,0 +1,3 @@
+# Go Azure SQL Database Servcer
+# 1. Login
+# 2. Run the SQL script in SQLDatabase/SQL/create_covid_tables_ddl.sql
